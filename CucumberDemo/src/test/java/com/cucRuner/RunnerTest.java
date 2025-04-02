2b34b354376f912959466fe18de3cf25cb8e0eb9
LAKSITHA29 <laksitharavi@gmail.com>
2025-04-02 10:40:46 +0530

    json commit

--- CucumberDemo/src/test/java/com/cucRuner/RunnerTest.java
+++ CucumberDemo/src/test/java/com/cucRuner/RunnerTest.java
@@ -15,7 +15,7 @@ package com.cucRuner;
 //package Runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/Cucumber.html"},features="src\\test\\resources\\com\\features\\Login.feature",glue="stepDefinitions")
+@CucumberOptions(plugin = {"pretty","json:target/cucumber-reports/CucumberHRM.json", "html:target/cucumber-reports/Cucumber.html"},features="src\\test\\resources\\com\\features\\Login.feature",glue="stepDefinitions")
 public class RunnerTest extends  AbstractTestNGCucumberTests{
 
 }
